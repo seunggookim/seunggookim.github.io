318f549daf049a7651e014b92f868ec07e07550f
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-09-16 09:40:11 +0200

    what about now?

--- assets/json/add_list.sh
+++ assets/json/add_list.sh
@@ -3,6 +3,6 @@
 cp resume.json resume.json.old
 LIST=`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | awk -F/ '{print $NF}' | sort -f | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g'`
 echo ${LIST}
-sed "s/<ADD_HERE>/\t\t\"summary\": \"${LIST}\"/g" resume-addhere.json > resume.json 
+sed "s/<ADD_HERE>/\t\t\"summary\": \"${LIST}\"/g" resume-addhere.json > resume-checkmeout.json 
 
 #sed "s/<ADD_HERE>/\t\t\"summary\": \"`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | awk -F/ '{print $NF}' | sort -f | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g'`\"/g" resume-addhere.json > resume.json 
