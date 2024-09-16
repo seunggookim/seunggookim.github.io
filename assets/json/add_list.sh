b461faa21fe4a52e92358eaca1ad12ce39ca558d
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-09-16 10:48:59 +0200

    json files without $schema line

---
+++ assets/json/add_list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#one-liner-magic generated using ChatGPT4o
+
+# create a backup file:
+sed "/$schema/d" resume.json > backup.json # this line needs to be removed not to confuse the remote builder
+
+# create a string to add:
+LIST=`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | awk -F/ '{print $NF}' | sort -f | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g'`
+echo ${LIST}
+
+# add the string:
+sed "s/<ADD_HERE>/\t\t\"summary\": \"${LIST}\"/g" addhere.json > resume.json 
+
+#sed "s/<ADD_HERE>/\t\t\"summary\": \"`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | awk -F/ '{print $NF}' | sort -f | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g'`\"/g" addhere.json > resume.json 
