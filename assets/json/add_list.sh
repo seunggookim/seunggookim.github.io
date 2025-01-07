5d5443b27f344e056f7c54fc92c76a304f31a823
Seung-Goo KIM <dr.seunggoo.kim@gmail.com>
2025-01-07 18:18:25 +0100

    resume+

--- assets/json/add_list.sh
+++ assets/json/add_list.sh
@@ -9,6 +9,6 @@ LIST=`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | a
 echo ${LIST}
 
 # add the string:
-sed "s/\"<ADD_HERE>\": \"\"/\"summary\": \"${LIST}\"/g" addhere.json > resume.json 
+sed "s/\"<ADD_HERE>\": \"\"/\"summary\": \"${LIST}\"/g" EDITME.json > resume.json 
 
 #sed "s/\"<ADD_HERE>\": \"\"/\"summary\": \"`find ~/Seafile/MyLibrary/Service/Reviews -depth 1 -type d ! -name '_*' | awk -F/ '{print $NF}' | sort -f | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g'`\"/g" addhere.json > resume.json 
